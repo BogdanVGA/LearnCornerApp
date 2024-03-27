19b0163de563dfa6dcb5121ed7b5aefdb77ee7fd
BogdanVGA <bogdan.vuluga@yahoo.com>
2024-03-27 20:39:57 +0200

    added Modal to UserProfile.tsx

--- src/layouts/UserProfile/UserProfile.tsx
+++ src/layouts/UserProfile/UserProfile.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { useOktaAuth } from "@okta/okta-react";
+import { Modal } from "../Utils/Modal";
 
 export const UserProfile = () => {
 
@@ -13,12 +14,11 @@ export const UserProfile = () => {
     const [username, setUsername] = useState('');
 
     const [isLoading, setIsLoading] = useState(true);
-    const [httpError, setHttpError] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [modalMessage, setModalMessage] = useState('');
 
     useEffect(() => {
-
         const fetchUserData = async () => {
-
             if (authState && authState.isAuthenticated) {
 
                 try {
@@ -48,9 +48,9 @@ export const UserProfile = () => {
                     setAccountType(userData.userRole);
                     setUsername(userData.username);
 
-
                 } catch (error: any) {
-                    setHttpError(error.message);
+                    setModalMessage(error.message);
+                    setShowModal(true);
                 } finally {
                     setIsLoading(false);
                 }
@@ -83,11 +83,12 @@ export const UserProfile = () => {
 
             const updatedUserData = await response.json();
             console.log(updatedUserData);
-            alert('Profile updated successfully!');
+            setModalMessage('Profile updated successfully!');
+            setShowModal(true);
 
         } catch (error: any) {
-            console.error('Error updating profile:', error);
-            setHttpError(error.message);
+            setModalMessage(error.message);
+            setShowModal(true);
         }
     };
 
@@ -97,74 +98,75 @@ export const UserProfile = () => {
         )
     }
 
-    if (httpError) {
-        return (
-            <div className='container m-5'>
-                <p>{httpError}</p>
-            </div>
-        )
+    const onCloseModal = () => {
+        setShowModal(false);
     }
 
     return (
-        <div className='container rounded bg-white mt-5 mb-5'>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className='row'>
-                    <div className='col-md-3 border-right'>
-                        <div className='d-flex flex-column align-items-center text-center p-3 py-5'>
-                            <img
-                                className='rounded-circle mt-5'
-                                width='150px'
-                                src='https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg'
-                                alt='User'>
-                            </img>
-                            <div className='col-md-6'>
-                                <label className='font-weight-bold'>{fullName}</label>
+        <div>
+            <Modal showModal={showModal} modalMessage={modalMessage} onClose={onCloseModal}/>
+            <div className='container rounded bg-white mt-5 mb-5'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className='row'>
+                        <div className='col-md-3 border-right'>
+                            <div className='d-flex flex-column align-items-center text-center p-3 py-5'>
+                                <img
+                                    className='rounded-circle mt-5'
+                                    width='150px'
+                                    src='https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg'
+                                    alt='User'>
+                                </img>
+                                <div className='col-md-6'>
+                                    <label className='font-weight-bold'>{fullName}</label>
+                                </div>
+                                <div>
+                                    <label className='text-black-80'>Account type: {accountType}</label>
+                                </div>
                             </div>
-                            <div>
-                                <label className='text-black-80'>Account type: {accountType}</label>
+                        </div>
+                        <div className='col-md-5 border-right'>
+                            <div className='p-3 py-5'>
+                                <div className='d-flex justify-content-between align-items-center mb-3'>
+                                    <h4 className='text-right'>Profile Settings</h4>
+                                </div>
+                                <div className='row mt-2'>
+                                    <div className='col-md-6'>
+                                        <label className='labels'>First Name</label>
+                                        <input {...register('firstName', { required: true })} type='text' className='form-control mt-1' />
+                                        {errors.firstName && <span className="text-danger">First name is required.</span>}
+                                    </div>
+                                    <div className='col-md-6'>
+                                        <label className='labels'>Last Name</label>
+                                        <input {...register('lastName', { required: true })} type='text' className='form-control mt-1' />
+                                        {errors.lastName && <span className="text-danger">Last name is required.</span>}
+                                    </div>
+                                </div>
+                                <div className='row mt-3'>
+                                    <div className='col-md-12'>
+                                        <label className='labels'>Username</label>
+                                        <input {...register('username', { required: true })} type='text' className='form-control mt-1' />
+                                        {errors.username && <span className="text-danger">Username is required.</span>}
+                                    </div>
+                                    <div className='col-md-12 mt-3'>
+                                        <label className='labels'>Email</label>
+                                        <input {...register('email', { required: true })} type='text' className='form-control mt-1' />
+                                        {errors.email && <span className="text-danger">Email is required.</span>}
+                                    </div>
+                                </div>
+                                <div className='mt-5 text-right'>
+                                    <button className='btn btn-outline-dark main-color text-white profile-button' type='submit'>
+                                        Save Profile
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className='col-md-5 border-right'>
-                        <div className='p-3 py-5'>
-                            <div className='d-flex justify-content-between align-items-center mb-3'>
-                                <h4 className='text-right'>Profile Settings</h4>
-                            </div>
-                            <div className='row mt-2'>
-                                <div className='col-md-6'>
-                                    <label className='labels'>First Name</label>
-                                    <input {...register('firstName', { required: true })} type='text' className='form-control mt-1' />
-                                    {errors.firstName && <span className="text-danger">First name is required.</span>}
-                                </div>
-                                <div className='col-md-6'>
-                                    <label className='labels'>Last Name</label>
-                                    <input {...register('lastName', { required: true })} type='text' className='form-control mt-1' />
-                                    {errors.lastName && <span className="text-danger">Last name is required.</span>}
-                                </div>
-                            </div>
-                            <div className='row mt-3'>
-                                <div className='col-md-12'>
-                                    <label className='labels'>Username</label>
-                                    <input {...register('username', { required: true })} type='text' className='form-control mt-1' />
-                                    {errors.username && <span className="text-danger">Username is required.</span>}
-                                </div>
-                                <div className='col-md-12 mt-3'>
-                                    <label className='labels'>Email</label>
-                                    <input {...register('email', { required: true })} type='text' className='form-control mt-1' />
-                                    {errors.email && <span className="text-danger">Email is required.</span>}
-                                </div>
-                            </div>
-                            <div className='mt-5 text-right'>
-                                <button className='btn btn-outline-dark main-color text-white profile-button' type='submit'>
-                                    Save Profile
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     );
 }
 
 /* https://freefrontend.com/bootstrap-profiles/ */
+/* https://react-hook-form.com/get-started#Registerfields */
+/* https://www.npmjs.com/package/throttle-typescript */
